7e9a018dc13036fb557b4247271258fbbc3b8b98
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-12-27 17:52:25 +0545

    javascript function, hoising, varialbles, call stack demo

---
+++ Namastey Javascript/basic lesson/index.js
@@ -0,0 +1,25 @@
+var x = 7;
+
+function getName() {
+    console.log("Namaste Javascript by Akshay Saini");
+}
+
+getName();
+console.log(x);
+
+first();
+second();
+
+function first () {
+    var a = 1;
+    console.log(a);
+}
+
+function second() {
+    var a = 2;
+    console.log(a);
+}
+
+first();
+second();
+first();
