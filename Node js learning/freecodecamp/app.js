6e59eb789565126c1a9eebaf957cd4447a688059
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-05-07 23:19:37 -0500

    added few practice codes

--- Node js learning/freecodecamp/app.js
+++ Node js learning/freecodecamp/app.js
@@ -1,11 +1,2 @@
-// modules
+require('./7-functionTesting') // it will automatically invoke that function call without exports
 
-const names = require('./4-firstModule'); // always start with ./
-const sayHi = require('./5-utils');
-
-console.log(names);
-console.log(sayHi);
-
-sayHi("nirmal");
-sayHi(names.john);
-sayHi(names.peter);
