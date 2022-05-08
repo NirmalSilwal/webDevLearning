39ac6ae97cee480fd98054ff45af804b87c1a15a
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-05-07 23:34:52 -0500

    added OS module demo

--- Node js learning/freecodecamp/app.js
+++ Node js learning/freecodecamp/app.js
@@ -1,1 +1,10 @@
+const os = require('os');
 
+// console.log(os);
+
+// info about current user
+const user = os.userInfo();
+console.log(user);
+
+// system uptime
+ console.log(`system uptime is ${os.uptime} seconds`);
