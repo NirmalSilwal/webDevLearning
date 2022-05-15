7eef334c67830d2bd7534bb0cbb447e84650a2db
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-05-15 10:06:47 -0500

    DOM basics practice

--- Angela Course/Web Development/Javascript basics/index.js
+++ Angela Course/Web Development/Javascript basics/index.js
@@ -4,4 +4,15 @@
 var yourname = prompt("what is your name");
 alert(`welcome to Javascript ${yourname} !!!`);
 
-alert(prompt('enter your tweet').slice(0,140))
+alert(prompt('enter your tweet').slice(0, 140))
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0)
+        console.log("Fizz Buzz");
+    else if (i % 3 == 0)
+        console.log("Fizz");
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(i);
+}
