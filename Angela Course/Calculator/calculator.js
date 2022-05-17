607aa6c1be0bbdbd35b89ab7445cd2ea7caa07c9
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-05-17 10:51:57 -0500

    added bmi calculator route and functionality

--- Angela Course/Calculator/calculator.js
+++ Angela Course/Calculator/calculator.js
@@ -1,14 +1,41 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.get("/", function(req, res){
+// we want our app to use the body parser now
+// urlencoded is the special mode of bodyparser that allows us to parse data from HTML form
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
-    res.send("Thanks for visiting the calculator website.");
+app.post("/", function (req, res) {
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let result = num1 + num2;
+
+    res.send("The result of calculation is: " + result);
+});
+
+app.get("/bmicalculator", function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+    let weight = Number(req.body.weight);
+    let height = Number(req.body.height);
+
+    let bmi = weight / Math.pow(height, 2);
+    console.log(bmi);
+
+    res.send("<h2>Your Body mass index is " + bmi + "</h2>");
 })
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("server started on PORT 3000");
 });
