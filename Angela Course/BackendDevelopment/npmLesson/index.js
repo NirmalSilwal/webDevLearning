074dcaebefeac7ffa1aac5d80d8f5db74e5092e5
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-05-16 21:45:29 -0500

    learned about using npm modules

---
+++ Angela Course/BackendDevelopment/npmLesson/index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes"); 
+const supervillains = require('supervillains');
+
+var mysuperhero = superheroes.random();
+
+console.log("superhero is: " +mysuperhero);
+
+console.log("super villain is: " + supervillains.random());
