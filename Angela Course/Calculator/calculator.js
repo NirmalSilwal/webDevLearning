2029a2f10a2a83b4a5db74118e26cda898bfb793
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-05-17 01:06:31 -0500

    POST request usage demo

--- Angela Course/Calculator/calculator.js
+++ Angela Course/Calculator/calculator.js
@@ -5,6 +5,10 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req, res){
+    res.send("Thanks for visiting the calculator website.");
+})
+
 app.listen(3000, function(){
     console.log("server started on PORT 3000");
 });
