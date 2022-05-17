fb8578f3a7ad55d79d318650a268c1af72b87f9a
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-05-16 23:52:39 -0500

    demo on routing concepts in express

--- Angela Course/BackendDevelopment/my-express-server/server.js
+++ Angela Course/BackendDevelopment/my-express-server/server.js
@@ -1,6 +1,15 @@
+const { response } = require("express");
 const express = require("express");
-
 const app = express();
+
+// this method defines what should happen when someone makes a get request to the home root
+app.get("/", function(request, response){
+    response.send("<h1>hello express!!</h1>");
+})
+
+app.get("/contact", function(req, res){
+    res.send("<h2>contact me at: nirmal@gmail.com</h2>");
+})
 
 app.listen(3000, function(){
     console.log("server started on port 3000");
